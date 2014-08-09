024f1d94704aa53df08655cced108687a7e14174
Varun Sayal <sayalvarun@gmail.com>
2014-08-09 16:14:59 -0400

    retry last linked redirect fix commit

---
+++ scripts/index.js
@@ -0,0 +1,9 @@
+/*$(window).bind("scroll", function() {
+	if ($(this).scrollTop() > 400) {
+		$("#whoAmI").fadeIn("slow");
+	}
+	
+	if ($(this).scrollTop() > 600) {
+		$('#introInfo').css('display', 'inline-block');
+	} 
+});*/
