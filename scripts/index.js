ef60ec54aef4482be4f517a9143f230509bcc1d5
Varun Sayal <sayalvarun@gmail.com>
2014-08-16 19:18:21 -0400

    added iPhone media query and fixed navbar movement bug

--- scripts/index.js
+++ scripts/index.js
@@ -1,8 +1,8 @@
 $(document).ready(function(){
 	$("#sendEmailLink").click(function(){
 		getExtra();
-
 	});
+	$('#navbar').followTo($("#main_container").offset().top -40);
 });
 
 
@@ -15,3 +15,24 @@ function getExtra(){
 	base = base + subjectBase + modalSubjectText + bodyBase + modalBodyText;
 	$("#sendEmailLink").attr('href',base);
 }
+
+var windw = this;
+
+$.fn.followTo = function ( pos ) {
+    var $this = this,
+        $window = $(windw);
+    
+    $window.scroll(function(e){
+        if ($window.scrollTop() > pos) {
+            $this.css({
+                position: 'absolute',
+                top: pos
+            });
+        } else {
+            $this.css({
+                position: 'fixed',
+                top: 0
+            });
+        }
+    });
+};
