947d72d447d19e297c14cde8d6be99906b05fc1c
Varun <sayalvarun@gmail.com>
2015-04-20 22:06:25 -0400

    Major changes in separating the site nicely

--- scripts/index.js
+++ scripts/index.js
@@ -3,8 +3,14 @@ $(document).ready(function(){
 		getExtra();
 	});
 	$('#navbar').followTo($("#main_container").offset().top -40);
+    var waypoint = new Waypoint({
+      element: document.getElementById('educationInfo'),
+      handler: function(direction) {
+        startCounters();
+      },
+      offset: 200
+    })
 });
-
 
 function getExtra(){
 	var base = "mailto:sayalvarun@gmail.com?";
@@ -14,6 +20,74 @@ function getExtra(){
 	var modalBodyText = $("#modalBody").val().split(' ').join('%20');
 	base = base + subjectBase + modalSubjectText + bodyBase + modalBodyText;
 	$("#sendEmailLink").attr('href',base);
+}
+
+function startCounters(){
+    $({countNum: $('#honSize').text()}).animate({countNum: 30}, {
+      duration: 4000,
+      easing:'linear',
+      step: function() {
+        $('#honSize').text(Math.floor(this.countNum));
+      },
+      complete: function() {
+        $('#honSize').text(this.countNum);
+      }
+    });
+
+    $({countNum: $('#csGPA').text()}).animate({countNum: 79}, {
+      duration: 4000,
+      easing:'linear',
+      step: function() {
+        $('#csGPA').text(Math.floor(this.countNum));
+      },
+      complete: function() {
+        $('#csGPA').text(this.countNum);
+      }
+    });
+
+    $({countNum: $('#amsGPA').text()}).animate({countNum: 81}, {
+      duration: 4000,
+      easing:'linear',
+      step: function() {
+        $('#amsGPA').text(Math.floor(this.countNum));
+      },
+      complete: function() {
+        $('#amsGPA').text(this.countNum);
+      }
+    });
+
+    $({countNum: $('#scholLen').text()}).animate({countNum: 4}, {
+      duration: 4000,
+      easing:'linear',
+      step: function() {
+        $('#scholLen').text(Math.floor(this.countNum));
+      },
+      complete: function() {
+        $('#scholLen').text(this.countNum);
+      }
+    });
+
+    $({countNum: $('#uschSize').text()}).animate({countNum: 8}, {
+      duration: 4000,
+      easing:'linear',
+      step: function() {
+        $('#uschSize').text(Math.floor(this.countNum));
+      },
+      complete: function() {
+        $('#uschSize').text(this.countNum);
+      }
+    });
+
+    $({countNum: $('#gradYear').text()}).animate({countNum: 2016}, {
+      duration: 4000,
+      easing:'linear',
+      step: function() {
+        $('#gradYear').text(Math.floor(this.countNum));
+      },
+      complete: function() {
+        $('#gradYear').text(this.countNum);
+      }
+    });
 }
 
 var windw = this;
@@ -36,3 +110,5 @@ $.fn.followTo = function ( pos ) {
         }
     });
 };
+
+
---
+++ scripts/interests.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+	$("#sendEmailLink").click(function(){
+		getExtra();
+	});
+	$('#navbar').followTo($("#main_container").offset().top -40);
+});
+
+
+function getExtra(){
+	var base = "mailto:sayalvarun@gmail.com?";
+	var subjectBase = "subject=";
+	var bodyBase = "&body="
+	var modalSubjectText = $("#modalSubject").val().split(' ').join('%20');
+	var modalBodyText = $("#modalBody").val().split(' ').join('%20');
+	base = base + subjectBase + modalSubjectText + bodyBase + modalBodyText;
+	$("#sendEmailLink").attr('href',base);
+}
